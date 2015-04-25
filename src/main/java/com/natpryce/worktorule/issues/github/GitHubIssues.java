cbd573e951d1d1c2150d9837fb4ceb897df3c4c4
Nat Pryce <sw@natpryce.com>
2015-04-26 00:41:25 +0100

    Set correct Accept header for GitHub API, and use charset signalled in content type header when parsing the response

--- src/main/java/com/natpryce/worktorule/issues/github/GitHubIssues.java
+++ src/main/java/com/natpryce/worktorule/issues/github/GitHubIssues.java
@@ -1,15 +1,22 @@
 package com.natpryce.worktorule.issues.github;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.natpryce.worktorule.IssueTracker;
 import com.scurrilous.uritemplate.URITemplate;
 
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Objects;
+import java.util.Optional;
 
 public class GitHubIssues implements IssueTracker {
     private static final URITemplate uriTemplate =
@@ -26,7 +33,44 @@ public class GitHubIssues implements IssueTracker {
 
     @Override
     public boolean isOpen(String issueId) throws IOException {
-        return Objects.equals(objectMapper.readTree(urlFor(issueId)).get("state").asText("unknown"), "open");
+        JsonNode jsonNode = getJsonFor(issueId);
+        return jsonNode.get("state").asText("unknown").equals("open");
+    }
+
+    private JsonNode getJsonFor(String issueId) throws IOException {
+        URL issueUrl = urlFor(issueId);
+        HttpURLConnection cx = (HttpURLConnection)issueUrl.openConnection();
+        cx.setRequestProperty("Accept", "application/vnd.github.v3+json");
+
+        int responseCode = cx.getResponseCode();
+        if (responseCode >= 300) {
+            throw new IOException("request to " + issueUrl + " failed with status " + responseCode);
+        }
+
+        Reader r = new InputStreamReader(new BufferedInputStream(cx.getInputStream()), parseCharset(cx));
+        try {
+            return objectMapper.readTree(r);
+        }
+        finally {
+            r.close();
+        }
+    }
+
+    private String parseCharset(HttpURLConnection cx) throws IOException {
+        return parseCharset(parseContentType(cx));
+    }
+
+    private String parseCharset(MimeType contentType) {
+        return Optional.ofNullable(contentType.getParameter("charset")).orElse("utf-8");
+    }
+
+    private MimeType parseContentType(HttpURLConnection cx) throws IOException {
+        String contentType = cx.getContentType();
+        try {
+            return new MimeType(contentType);
+        } catch (MimeTypeParseException e) {
+            throw new IOException("failed to parse content type: " + contentType, e);
+        }
     }
 
     private URL urlFor(String issueId) {
