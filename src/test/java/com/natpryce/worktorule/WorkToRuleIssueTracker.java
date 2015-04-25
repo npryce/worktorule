a48378cb4d0b9fd99094afdd3914022d661f43e4
Nat Pryce <sw@natpryce.com>
2015-04-25 21:51:52 +0100

    Cache lookup of this project's own issues during a test run

--- src/test/java/com/natpryce/worktorule/WorkToRuleIssueTracker.java
+++ src/test/java/com/natpryce/worktorule/WorkToRuleIssueTracker.java
@@ -1,7 +1,8 @@
 package com.natpryce.worktorule;
 
+import com.natpryce.worktorule.internal.IssueCache;
 import com.natpryce.worktorule.issues.github.GitHubIssues;
 
 public class WorkToRuleIssueTracker {
-    public static final IssueTracker PUBLIC = new GitHubIssues("npryce", "worktorule");
+    public static final IssueTracker PUBLIC = new IssueCache(new GitHubIssues("npryce", "worktorule"));
 }
