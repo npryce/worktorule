828fc77871719b1877d552425507f20882caf728
Nat Pryce <sw@natpryce.com>
2015-04-25 23:53:19 +0100

    Removed unused import

--- src/main/java/com/natpryce/worktorule/issues/bitbucket/BitbucketIssues.java
+++ src/main/java/com/natpryce/worktorule/issues/bitbucket/BitbucketIssues.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Objects;
 
 public class BitbucketIssues implements IssueTracker {
     private static final URITemplate uriTemplate =
