465bd05165429f1e20389e64303a1eebde76fcea
Nat Pryce <sw@natpryce.com>
2015-04-26 22:41:29 +0100

    Delete unused constant

--- src/main/java/com/natpryce/worktorule/http/HttpConnectionSetting.java
+++ src/main/java/com/natpryce/worktorule/http/HttpConnectionSetting.java
@@ -4,10 +4,4 @@ import java.net.HttpURLConnection;
 
 public interface HttpConnectionSetting {
     void applyTo(HttpURLConnection cx);
-
-    HttpConnectionSetting NONE = new HttpConnectionSetting() {
-        @Override
-        public void applyTo(HttpURLConnection cx) {
-        }
-    };
 }
