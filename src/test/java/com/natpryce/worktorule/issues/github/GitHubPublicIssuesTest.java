0a4b6be8cbb940d8860136ca299bb94b3fef527b
Nat Pryce <na.pryce@net-a-porter.com>
2015-04-27 13:59:32 +0100

    Check GitHub API rate limits before test and skip test if rate limit exceeded

--- src/test/java/com/natpryce/worktorule/issues/github/GitHubPublicIssuesTest.java
+++ src/test/java/com/natpryce/worktorule/issues/github/GitHubPublicIssuesTest.java
@@ -1,14 +1,29 @@
 package com.natpryce.worktorule.issues.github;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.AssumptionViolatedException;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GitHubPublicIssuesTest {
     GitHubIssues issues = new GitHubIssues("npryce", "worktorule-testing");
+
+    @Before
+    public void checkRateLimit() throws IOException {
+        JsonNode rateLimitNode = new ObjectMapper().readTree(new URL("https://api.github.com/rate_limit"));
+        int requestsRemaining = rateLimitNode.findPath("resources").findPath("core").findPath("remaining").intValue();
+
+        if (requestsRemaining <= 0) {
+            throw new AssumptionViolatedException("API rate limit exceeded");
+        }
+    }
 
     @Test
     public void openIssue() throws IOException {
