c1cf01c8bc1ca48be36b839973f4e1f46e342948
Nat Pryce <sw@natpryce.com>
2016-04-07 09:10:49 +0100

    Delete unused code

--- src/test/java/com/natpryce/worktorule/WorkToRuleIssueTracker.java
+++
@@ -1,8 +0,0 @@
-package com.natpryce.worktorule;
-
-import com.natpryce.worktorule.issues.github.GitHubIssues;
-
-public class WorkToRuleIssueTracker {
-    public static final IssueTracker PUBLIC =
-            new GitHubIssues("npryce", "worktorule").cached();
-}
